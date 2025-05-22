51b5c9d667e34fb97ec00f7c73f35038606db273
Jeff Young <jeffyoung1990@gmail.com>
2025-05-22 12:43:50 -0600

    init

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CU
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone --depth 1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSH_CUSTOM/plugins/fast-syntax-highlighting
 git clone --depth 1 https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+sed -i 's/robbyrussell/clean/' ~/.zshrc
+sed -i 's/(git)/(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)/' ~/.zshrc
